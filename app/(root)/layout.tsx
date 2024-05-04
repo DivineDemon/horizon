fcce984dc4e672b2c933c17c3f5aae725dfb35d3
DivineDemon <supame123@gmail.com>
2024-05-04 13:41:34 +0400

    Sidebar!

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,18 @@
+import Sidebar from "@/components/Sidebar";
+
 const RootLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
+  const loggedIn = {
+    firstName: "Mushood",
+    lastName: "Hanif",
+  };
+
   return (
-    <main>
-      <div className="text-3xl font-bold underline">Sidebar</div>
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
       {children}
     </main>
   );
