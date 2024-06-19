31e87df382320bf9efaa7014171d9b5a0292ac7a
DivineDemon <supame123@gmail.com>
2024-06-19 15:20:38 +0400

    Cards + Layout!

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
+import Rightbar from "@/components/Rightbar";
 import HeaderBox from "@/components/HeaderBox";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
   const loggedIn = {
     firstName: "Mushood",
+    lastName: "Hanif",
+    email: "supame123@gmail.com",
   };
   return (
     <section className="home">
@@ -22,6 +25,11 @@ const Home = () => {
           />
         </header>
       </div>
+      <Rightbar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500 }]}
+      />
     </section>
   );
 };
